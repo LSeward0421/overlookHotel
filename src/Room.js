aaef684089f727db4d2c3f527e8423900aee02fd
Lauren Seward <lauren.unal@gmail.com>
2023-03-01 15:48:31 -0600

    Add basic class properties/methods for Room

---
+++ src/Room.js
@@ -0,0 +1,19 @@
+class Room {
+  constructor(roomData) {
+    this.number = roomData.number;
+    this.roomType = roomData.roomType;
+    this.bidet = roomData.bidet;
+    this.bedSize = roomData.bedSize;
+    this.numBeds = roomData.numBeds;
+    this.costPerNight = roomData.costPerNight;
+    this.isBooked = false;
+  }
+  getBooked() {
+    this.isBooked = true;
+  }
+  cancelBooking() {
+    this.isBooked = false;
+  }
+};
+
+export default Room;
--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,7 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 import Customer from './Customer';
+import Room from './Room';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
