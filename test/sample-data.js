44d0f74753e995fb301ee47a7b93ccc4dd9cda99
Lauren Seward <lauren.unal@gmail.com>
2023-03-01 17:36:20 -0600

    Create sample data for tests

---
+++ test/sample-data.js
@@ -0,0 +1,65 @@
+export const rooms = [
+  {
+    "number": 1,
+    "roomType": "residential suite",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 358.4
+  },
+  {
+    "number": 2,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 477.38
+  },
+  {
+    "number": 3,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "king",
+    "numBeds": 1,
+    "costPerNight": 491.14
+  }
+];
+
+export const bookings = [
+  {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 9,
+    "date": "2022/04/22",
+    "roomNumber": 15,
+
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 43,
+    "date": "2022/01/24",
+    "roomNumber": 24,
+
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2022/01/10",
+    "roomNumber": 12,
+
+  }
+];
+
+export const customers = [
+  {
+    "id": 1,
+    "name": "Leatha Ullrich"
+  },
+  {
+    "id": 2,
+    "name": "Rocio Schuster"
+  },
+  {
+    "id": 3,
+    "name": "Kelvin Schiller"
+  }
+];
