998aafc81142e9de29c8725b23a15df4eff04b93
Lauren Seward <lauren.unal@gmail.com>
2023-03-01 16:10:20 -0600

    Add basic booking class props and methods

---
+++ src/Booking.js
@@ -0,0 +1,24 @@
+class Booking {
+  constructor(id, userId, date, roomNumber){
+    this.id = id;
+    this.userId = userId;
+    this.date = date;
+    this.roomNumber = roomNumber;
+  }
+
+  getBookingDetails() {
+    // find the room number and set to this.roomNumber
+    // calculate the price
+    // create a booking object for each room property???
+  }
+  calculateBookingPrice() {
+    // this.roomNumber 
+    // only book 1 night
+    // 
+  }
+  cancelBooking() {
+
+  }
+}
+
+export default Booking;
--- src/scripts.js
+++ src/scripts.js
@@ -5,6 +5,7 @@
 import './css/styles.css';
 import Customer from './Customer';
 import Room from './Room';
+import Booking from './Booking';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
